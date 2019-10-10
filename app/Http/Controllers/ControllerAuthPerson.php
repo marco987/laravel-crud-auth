afffdf4eaec5797becc007867b1ed03b6f3ca7f8
marco987 <pacemarco87@gmail.com>
2019-10-10 14:43:58 +0200

    HTML + CSS base

--- app/Http/Controllers/ControllerAuthPerson.php
+++ app/Http/Controllers/ControllerAuthPerson.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class ControllerAuthPerson extends Controller
 {
--- app/Http/Controllers/ControllerPerson.php
+++ app/Http/Controllers/ControllerPerson.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class ControllerPerson extends Controller
 {
-    //
+  public function index() {
+    
+    $people = Person::all();
+
+    return view('home', compact('people'));
+  }
 }
